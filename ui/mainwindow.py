155f8df30a6b409b5e4caa393fef045e0efacc02
박성재 <p030610@naver.com>
2022-05-09 17:36:30 +0900

    calendar, loginpage, registerpage file add

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -5,10 +5,7 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 import PyQt6.QtWidgets
 
-
-
 class MainWindow(QWidget):
-    #버튼으로 대전략,상세전략 정하고 메인 대시보드로 돌입
     def __init__(self, parent=None):
         super().__init__()
         self.initUI()
